2ca4c5a766c6104043b9c940f23141e3042796ca
NazarL <nazar.lebedenko@gmail.com>
2021-03-13 13:36:21 -0500

    add styles, components and contacts  files

--- src/index.js
+++ src/index.js
@@ -22,6 +22,18 @@ ReactDOM.render(
       phone="+123 456 789"
       email="b@beyonce.com"
     />
+    <Card
+      name="Jack Bauer"
+      image="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      phone="+987 654 321"
+      email="jack@nowhere.com"
+    />
+    <Card
+      name="Chuck Norris"
+      image="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+      phone="+918 372 574"
+      email="gmail@chucknorris.com"
+    />
 
     {/* <h2>Jack Bauer</h2>
     <img
