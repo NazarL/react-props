eaa8a4ddc43e7769d9b51ebd25ee862b78448c2f
NazarL <nazar.lebedenko@gmail.com>
2021-03-13 13:22:44 -0500

    sample contact Card with props

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Card(props) {
+  console.log(props);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.image} alt="avatar_img" />
+      <p>{props.phone}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
-
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar_img"
+    <Card
+      name="Beyonce"
+      image="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      phone="+123 456 789"
+      email="b@beyonce.com"
     />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
 
-    <h2>Jack Bauer</h2>
+    {/* <h2>Jack Bauer</h2>
     <img
       src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
       alt="avatar_img"
@@ -27,7 +37,7 @@ ReactDOM.render(
       alt="avatar_img"
     />
     <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
+    <p>gmail@chucknorris.com</p> */}
   </div>,
   document.getElementById("root")
 );
