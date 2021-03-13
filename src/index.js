f442d79a45eb733cb9ac4037f8305175af929c67
NazarL <nazar.lebedenko@gmail.com>
2021-03-13 15:10:04 -0500

    add reusable Card

--- src/index.js
+++ src/index.js
@@ -1,55 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function Card(props) {
-  console.log(props);
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.image} alt="avatar_img" />
-      <p>{props.phone}</p>
-      <p>{props.email}</p>
-    </div>
-  );
-}
-
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-    <Card
-      name="Beyonce"
-      image="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      phone="+123 456 789"
-      email="b@beyonce.com"
-    />
-    <Card
-      name="Jack Bauer"
-      image="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      phone="+987 654 321"
-      email="jack@nowhere.com"
-    />
-    <Card
-      name="Chuck Norris"
-      image="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      phone="+918 372 574"
-      email="gmail@chucknorris.com"
-    />
-
-    {/* <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
-    />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
-
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
-    />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p> */}
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
